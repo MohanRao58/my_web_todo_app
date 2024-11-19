63917d94baa136b42ce789c6becfc9446a3b40a2
MohanRao58 <mohanraoganala7@gmail.com>
2024-11-19 14:38:54 +0530

    Implement "Complete todo items feature"

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ st.title("My Todo App")
 st.subheader("This is my Todo App")
 st.write("This app is to increase productivity")
 
-todos = functions.get_todos()
-for todo in todos:
-    st.checkbox(todo)
-
+for index,todo in enumerate(todos):
+    check_box = st.checkbox(todo,key=todo)
+    if check_box:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 st.text_input(label="",placeholder="Add a new todo...",on_change=add_todo,key='new_todo')
